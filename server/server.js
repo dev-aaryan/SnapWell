460bffc68938caa88fc702ce88231e30117b318a
dev-aaryan <aaryanguptaiit@gmail.com>
2024-09-20 14:10:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const path = require('path');
 
 const dbConfig = require('./config/dbConfig')
 app.use(express.json());
